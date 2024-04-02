7b0055944a4c8a374438c9852fee11ed341e61bf
rasysbox <rasysbox@hotmail.com>
2024-04-02 18:13:27 -0500

    vercel json

--- config/database.config.js
+++ config/database.config.js
@@ -2,5 +2,12 @@ module.exports = {
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD,
+    dialect: "mysql2",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
 }
--- models/connection.js
+++ models/connection.js
@@ -1,11 +1,26 @@
 const { Sequelize } = require('sequelize');
+const mysql2 = require('mysql2');
 const config = require('../config/database.config'); 
 
-const sequelize = new Sequelize(config.database, config.username, config.password, {
-    host: config.host,
-    dialect: 'mysql',
-    operatorsAliases: 'false',
-    logging: false
-});  
+const sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    {
+        host: config.host,
+        dialect: 'mysql',
+        dialectModule: mysql2,
+        port: 3306,
+        operatorsAliases: 'false',
+        logging: false,
+        benchmark: true,
+        pool: {
+            max: config.pool.max,
+            min: config.pool.min,
+            acquire: config.pool.acquire,
+            idle: config.pool.idle
+        }
+    }
+);
 
 module.exports = sequelize
